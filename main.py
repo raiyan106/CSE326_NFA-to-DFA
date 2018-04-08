375309d2702a92d5aff9368b933ffa8d5623ddcc
raiyan106 <raiyan.khan.106@gmail.com>
2018-04-09 04:17:14 +0600

    Still buggy :(

--- main.py
+++ main.py
@@ -34,22 +34,31 @@ Bptr=-1
 States = [state for i in range(STATES)]
 temp = [None]*(STATES+1)
 inp = [None]*10
-nos=noi=nof=j=k=nods=-1
+nos=None
+noi=None
+nof=None
+nods=-1
+
+#global nos, noi, nof, nods
 
 def pushA(z):
+    global Aptr
     Aptr+=1
     stackA[Aptr]=z
 
 def pushB(z):
+    global Bptr
     Bptr+=1
     stackB[Bptr]=z
 
 def popA():
+    global Aptr
     val = stackA[Aptr]
     Aptr-=1
     return val
 
 def copy(i):
+    global Bptr
     temp=[None]*(STATES+1)
     temp[0]=" "
     k=0
@@ -61,14 +70,17 @@ def copy(i):
 
 
 def popB():
+    global Bptr
     val = stackB[Bptr]
     Bptr-=1
     return val
 
 def peekB():
+    global Bptr
     return stackA[Bptr]
 
 def peekA():
+    global Aptr
     return stackA[Aptr]
 
 def seek(arr,ptr,s):
@@ -78,21 +90,27 @@ def seek(arr,ptr,s):
     return 0
 
 def sort():
+    global Bptr
     tempo=None
     for i in range(Bptr):
-        for j in range(Bptr-1):
+        for j in range(Bptr-i):
             if stackB[j]>stackB[j+1]:
                 tempo=stackB[j]
                 stackB[j]=stackB[j+1]
                 stackB[j+1]=tempo
 
 def toString():
+    global Bptr
     sort();
     for i in range(Bptr+1):
         temp[i]=str(stackB[i])
-    temp[i]=None
+    temp[i+1]=None
 
 def display_DTran():
+    global Aptr
+    global Bptr
+    global nos, noi, nof, nods
+
     print("\n\t\t DFA Transition Table ")
     print("\n\t\t ---------------------")
     print("\nStates\tString\tInputs\n ")
@@ -121,6 +139,9 @@ def move(st,j):
 
 
 def lambda_closure(st):
+    global Aptr
+    global Bptr
+    global nos, noi, nof, nods
     ctr = 0
     in_state=st
     curst = st
@@ -138,3 +159,107 @@ def lambda_closure(st):
             chk = seek(stackA,Aptr,in_state)
             if(chk==0 and ctr<=States[curst]['tranlist'][noi]['notran']):
                 pushA(in_state)
+
+#Main Code
+
+print("\tNFA TO DFA CONVERTION\n\n")
+final=[None]*20
+fin = 0
+c = None
+ans = None
+st = [None]*20
+
+nos = input("Enter no. of States in NFA : ")
+nos = int(nos)
+
+for i in range(nos):
+    States[i]['no']=i
+
+start = input("Enter the start state : ")
+start = int(start)
+
+nof = input("Enter the number of final state : ")
+nof = int(nof)
+
+print("\nEnter the final state : \n")
+'''
+for i in range(nof):
+    val = input()
+    final[i] = int(val)
+'''
+final=map(int,input().split())
+final=list(final)
+noi = input("\nEnter the number of input symbols : ")
+noi = int(noi)
+
+print("\nEnter the input symbols : \n")
+'''
+for i in range(noi):
+    val = input()
+    inp[i] = val
+inp[i+1]='e'
+'''
+inp=map(str,input().split())
+inp=list(inp)
+inp.append('e')
+
+print("\nEnter the transitions : (-1 to stop)\n")
+for i in range(nos):
+    for j in range(noi):
+        States[i]['tranlist'][j]['sym']=inp[j]
+        k=0
+        ans='y'
+        while(ans=='y'):
+            val = input("move({}, {}) : ".format(i,inp[j]))
+            val = int(val)
+            States[i]['tranlist'][j]['tostates'][k] = val
+            k+=1
+            if(States[i]['tranlist'][j]['tostates'][k-1]==-1):
+                k-=1
+                ans='n'
+                break
+        
+        States[i]['tranlist'][j]['notran'] = k
+
+i=nods=fin=0
+pushA(start)
+lambda_closure(peekA())
+toString()
+Dstates[nods]['name']='A'
+nods+=1
+Dstates[0]['StateString'] = list(temp)
+
+while(i<nods):
+    for j in range(noi):
+        fin = 0
+        copy(i)
+        while(Bptr!=-1):
+            move(popB(),j)
+        while(Aptr!=-1):
+            lambda_closure(peekA())
+        toString()
+        for k in range(nods):
+            if temp==Dstates[k]['StateString']:
+                Dstates[i]['trans'][j] = Dstates[k]['name']
+                break
+        
+        if k==nods:
+            nods+=1
+            for k in range(nof):
+                fin = seek(stackB,Bptr,final[k])
+                if fin==1:
+                    Dstates[nods-1]['is_final'] = 1
+                    break
+            
+            Dstates[nods-1]['StateString'] = list(temp)
+            convert = ord('A')
+            convert=convert+(nods-1)
+            convert=chr(convert)
+            Dstates[nods-1]['name'] = convert
+            Dstates[i]['trans'][j] = Dstates[nods-1]['name']
+    
+    i=i+1
+
+display_DTran()
+
+
--- play.py
+++ play.py
@@ -1,7 +1,37 @@
-a=" "*5
+#a=" "*5
 #a[2]='r'
 #print(int('5')-0)
-v=5
 
+class Integer:
+    def __init__(self,value):
+        self.value = value
 
-print("Value is {}".format(v))
+    def increase(self):
+        self.value+=1
+    def decrease(self):
+        self.value-=1
+
+lst=[None]*5
+#global a
+a= Integer(-1)
+i = -1
+j=0
+def push(z):
+    #global a
+    #a.increase()
+    global i
+    i+=1
+    lst[i]=z
+def pop():
+    global i, j
+    val = lst[i]
+    #a.decrease()
+    i-=1
+    j+=1
+    return val
+
+push(55)
+push(56)
+g = pop()
+#f = pop()
+print(i, j)
